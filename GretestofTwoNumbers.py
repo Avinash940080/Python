e736ce9c5206f5815c88cfe7f95ae9347479e6d5
Avinash940080 <161922549+Avinash940080@users.noreply.github.com>
2024-07-05 01:00:15 +0530

    Delete GretestofTwoNumbers.py

--- GretestofTwoNumbers.py
+++
@@ -1,6 +0,0 @@
-def greatest_of_two(a, b):
-    return max(a, b)
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-print("Greatest of the two numbers:", greatest_of_two(num1, num2))
