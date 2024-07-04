6806548c68084ff55b0943c427637271b7c33e63
Avinash940080 <161922549+Avinash940080@users.noreply.github.com>
2024-07-05 01:00:39 +0530

    Delete Lower case or Upper case.py

--- Lower case or Upper case.py
+++
@@ -1,9 +0,0 @@
-ch=input("Enter the charecter")
-if(ch.islower()):
-print(ch," is lower case alphabet")
-elif(ch.isupper()):
-print(ch, " is Upper case alphabet")
-elif(ord(ch)>=48 and ord(ch)<=57):
-print(ch, " is digit")
-else:
-print(ch, " is special charecter")
